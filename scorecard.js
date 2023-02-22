f85dc7c06527387b2c6abfb431cadd73330ac0b2
Luke Lai <lukelai.dev@gmail.com>
2023-02-22 14:50:55 +0000

    Finish TDD showCurrentRoll and updateFrame methods

--- scorecard.js
+++ scorecard.js
@@ -1,8 +1,14 @@
+const Frame = require("./frame");
+
 class ScoreCard {
   constructor() {
-    this.currentRoll = 0;
+    this.currentRoll = 1;
     this.frames = [];
     this.totalScore = 0;
+  }
+
+  showCurrentRoll() {
+    return this.currentRoll;
   }
 
   addFrame(frame) {
@@ -12,6 +18,34 @@ class ScoreCard {
   showFrames() {
     return this.frames;
   }
+
+  updateFrame(roll, pins) {
+    const currentFrame = this.frames[this.frames.length - 1];
+    if (!currentFrame.showFirstKnock()) {
+      currentFrame.inputFirstKnock(pins);
+      this.currentRoll += 0.5;
+    } else if (!currentFrame.showSecondKnock()) {
+      currentFrame.inputSecondKnock(pins);
+      this.currentRoll += 0.5;
+    }
+
+    if (currentFrame.showFirstKnock() && currentFrame.showSecondKnock()) {
+      currentFrame.checkIsStrikes(); //check and change to true if strikes
+      currentFrame.checkIsSpares(); //check and changes to true if spares
+    }
+
+    if (currentFrame.checkIsStrikes()) {
+      this.currentRoll = Math.ceil(this.currentRoll);
+    }
+  }
 }
 
 module.exports = ScoreCard;
+
+// const scoreCard = new ScoreCard();
+// const frame = new Frame();
+//
+// scoreCard.addFrame(frame);
+// scoreCard.updateFrame(1, 6);
+// scoreCard.updateFrame(1, 4);
+// console.log(scoreCard.showFrames());
--- scorecard.test.js
+++ scorecard.test.js
@@ -18,4 +18,45 @@ describe("ScoreCard", () => {
     expect(scorecard.showFrames().length).toEqual(1);
     expect(scorecard.showFrames()[0].showFirstKnock()).toEqual(10);
   });
+
+  it("updates the latest frame knock when first try on roll 1", () => {
+    scorecard.addFrame(mockFrame);
+    scorecard.updateFrame(1, 7);
+    mockFrame.showFirstKnock.mockImplementation(() => 7);
+    expect(scorecard.showFrames()[0].showFirstKnock()).toEqual(7);
+  });
+
+  it("updates the latest frame knock when second try on roll 1", () => {
+    scorecard.addFrame(mockFrame);
+    scorecard.updateFrame(1, 7);
+    scorecard.updateFrame(1, 2);
+    mockFrame.showFirstKnock.mockImplementation(() => 7);
+    mockFrame.showSecondKnock.mockImplementation(() => 2);
+    expect(scorecard.showFrames()[0].showFirstKnock()).toEqual(7);
+    expect(scorecard.showFrames()[0].showSecondKnock()).toEqual(2);
+  });
+
+  it("shows current roll after first try", () => {
+    scorecard.addFrame(mockFrame);
+    scorecard.updateFrame(1, 7);
+    mockFrame.showFirstKnock.mockImplementation(() => 7);
+    expect(scorecard.showCurrentRoll()).toEqual(1.5);
+  });
+
+  it("shows current roll after second try", () => {
+    scorecard.addFrame(mockFrame);
+    scorecard.updateFrame(1, 7);
+    scorecard.updateFrame(1, 2);
+    mockFrame.showFirstKnock.mockImplementation(() => 7);
+    mockFrame.showSecondKnock.mockImplementation(() => 2);
+    expect(scorecard.showCurrentRoll()).toEqual(2);
+  });
+
+  it("moves to next roll after first strikes", () => {
+    mockFrame.showFirstKnock.mockImplementation(() => 10);
+    mockFrame.checkIsStrikes.mockImplementation(() => true);
+    scorecard.addFrame(mockFrame);
+    scorecard.updateFrame(1, 10);
+    expect(scorecard.showCurrentRoll()).toEqual(2);
+  });
 });
